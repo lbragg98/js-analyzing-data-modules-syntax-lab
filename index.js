2c96f3e846184b76a644f51705c81ad856036c90
Logan <lbragg98@gmail.com>
2025-11-17 15:39:01 -0500

    lab5 done

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 
+function combineUsers(...args) {
+  let combinedObject = {
+    users: []
+      }
 
+      for (const array of args) {
+        combinedObject.users = [
+            ...combinedObject.users,
+            ...array
+          ];
+      }
 
+    combinedObject.merge_date = new Date().toString("M/d/yyyy");
+
+      return combinedObject;
+ }
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+//test 
+console.log(combineUsers(
+  ["amy", "bobert"],
+  ["amy2"],
+  ["diana", "erico"]
+));
